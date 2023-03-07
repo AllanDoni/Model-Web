df0f6f35eb81bc2c6d44a42a9ad4bd9fc0a59764
Allan Doni <90502168+AllanDoni@users.noreply.github.com>
2023-03-07 11:24:26 +0530

    Add files via upload

---
+++ js/show.js
@@ -0,0 +1,14 @@
+// JavaScript Document
+var a
+function show_hide()
+{
+	if(a==1)
+		{
+			document.getElementById("show").style.display="inline";
+			return a=0;
+		}
+	else{
+		document.getElementById("show").style.display="inline";
+			return a=1;
+	}
+}
